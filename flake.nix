c4a0eed97cd82cd342b7d7b0b4906c60eff81ae8
natsukium <tomoya.otabi@gmail.com>
2024-10-18 00:12:39 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,138 @@
+{
+  description = "My slides";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    typix = {
+      url = "github:loqusion/typix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    typst-packages = {
+      url = "github:typst/packages";
+      flake = false;
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      imports = with inputs; [
+        git-hooks.flakeModule
+        treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          typixLib = inputs.typix.lib.${system};
+
+          typstPackagesSrc = pkgs.symlinkJoin {
+            name = "typst-packages-src";
+            paths = [
+              "${inputs.typst-packages}/packages"
+            ];
+          };
+
+          typstPackagesCache = pkgs.stdenvNoCC.mkDerivation {
+            name = "typst-packages-cache";
+            src = typstPackagesSrc;
+            dontBuild = true;
+            installPhase = ''
+              mkdir -p "$out/typst/packages"
+              cp -LR --reflink=auto --no-preserve=mode -t "$out/typst/packages" "$src"/*
+            '';
+          };
+
+          commonArgs = {
+            typstSource = "main.typ";
+
+            fontPaths = [
+              "${pkgs.ipaexfont}/share/fonts/opentype"
+            ];
+          };
+
+          build-drv =
+            src:
+            typixLib.buildTypstProject (
+              commonArgs
+              // {
+                src = typixLib.cleanTypstSource src;
+                virtualPaths = [ src ];
+                env.XDG_CACHE_HOME = typstPackagesCache;
+              }
+            );
+
+          watch-script = typixLib.watchTypstProject commonArgs;
+        in
+        {
+          apps = {
+            default = config.apps.watch;
+            watch = {
+              type = "app";
+              program = lib.getExe watch-script;
+            };
+          };
+
+          devShells.default = typixLib.devShell {
+            inherit (commonArgs) fontPaths;
+            packages = [
+              watch-script
+              pkgs.zathura
+            ];
+            shellHook = config.pre-commit.installationScript;
+          };
+
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              src = ./.;
+              hooks = {
+                actionlint.enable = true;
+                treefmt.enable = true;
+                typos.enable = true;
+              };
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              yamlfmt.enable = true;
+            };
+            settings.formatter = {
+              typstyle = {
+                command = "${lib.getExe pkgs.typstyle}";
+                includes = [ "*.typ" ];
+              };
+            };
+          };
+        };
+    };
+}
